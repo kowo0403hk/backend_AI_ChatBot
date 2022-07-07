8e04aaae43f1fcc0f5b9e46968ef0ed3f8c492f2
Alex Cheung <alexc.sfu@gmail.com>
2022-07-07 06:48:29 +0000

    finished initial setup

---
+++ index.ts
@@ -0,0 +1,18 @@
+require("dotenv").config();
+import express from "express";
+import morgan from "morgan";
+const app = express();
+
+// import Routers
+import openaiRouter from "./Routers/openaiRoute";
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// router to text to speach
+app.use("/api/openai", openaiRouter());
+
+app.listen(8080, () => {
+  console.log("backend listening on port 8080");
+});
